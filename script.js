6474b3a6a08afbca6b200d0f761cdfedc8f2ea5d
AmalStudentWepro <163731290+AmalStudentWepro@users.noreply.github.com>
2025-01-31 14:26:28 +0300

    Second uploud site

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// дз1
+//дз 1
 let nums = [1, 2, 5, 6, 22, 44, 66, 3, 4, 17, 22, 444]
-let suma = nums.reduce((total, num) => total + num, 0)
+
+let suma = 0 
+nums.forEach(num => suma += num)
 
 console.log("Сумма:", suma)
 
-// дз2
+// дз 2
 let arr = ["hello", 1234, "", false, NaN, 123, true, " ", null, "29.01.1992"]
 
 let string = []
@@ -12,7 +14,7 @@ let number = []
 let object = []
 let boolean = []
 
-for (let item of arr) {
+arr.forEach(item => {
   if (typeof item === "string") {
     string.push(item)
   } else if (typeof item === "number" && !isNaN(item)) {
@@ -22,9 +24,9 @@ for (let item of arr) {
   } else {
     object.push(item)
   }
-}
+})
 
-console.log(string)  
-console.log(number) 
+console.log(string)
+console.log(number)
 console.log(boolean)
-console.log(object) 
+console.log(object)
